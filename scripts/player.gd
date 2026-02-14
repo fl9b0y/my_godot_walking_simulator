2d6ea5d6f903336734cc4c185d0661d56bdecec6
Fl9b0y <jmuda918@gmail.com>
2026-02-14 20:48:34 +0200

    Added sprinting mechanic

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,13 +16,18 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("Left", "Right", "Forwards", "Backwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+		
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		if Input.is_action_pressed("Sprint"):
+			velocity.z = (direction.z * SPEED) *7
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
 
 	move_and_slide()
