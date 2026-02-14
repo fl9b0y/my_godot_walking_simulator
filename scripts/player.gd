ac22ae79ecb1d4cb74cb5140979f30fa8c820b85
Fl9b0y <jmuda918@gmail.com>
2026-02-14 21:16:40 +0200

    added mouse controls

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,9 +4,15 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@export var sensitivity = 1000
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -18,6 +24,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("Left", "Right", "Forwards", "Backwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 
 		
 	if direction:
@@ -31,3 +38,11 @@ func _physics_process(delta: float) -> void:
 	
 
 	move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation.y -= event.relative.x /sensitivity 
+		
+		$camera_pivot.rotation.x -= event.relative.y/sensitivity 
+		$camera_pivot.rotation.x =clamp($camera_pivot.rotation.x,deg_to_rad(-45),deg_to_rad(90))
+		
